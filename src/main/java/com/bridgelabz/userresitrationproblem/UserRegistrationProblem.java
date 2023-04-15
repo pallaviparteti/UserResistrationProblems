e24ea24c3e4f0d7dcb4735611981b7564d8597cf
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-04-15 13:41:02 +0530

    Add: Lmba Expression for valid user entry

--- src/main/java/com/bridgelabz/userresitrationproblem/UserRegistrationProblem.java
+++ src/main/java/com/bridgelabz/userresitrationproblem/UserRegistrationProblem.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.userresitrationproblem;
 import java.util.Scanner;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,67 +9,41 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
 
-	private String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-	private Pattern pattern = Pattern.compile(regex);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-	public void validateFirstName(String firstName) throws InvalidFirstNameException {
-		if (firstName.isEmpty()) {
-			throw new InvalidFirstNameException("Invalid first name");
-		}
+		String firstName = getUserInput(scanner, "Enter first name: ", s -> s.matches("[A-Za-z]+"),
+				"Invalid first name!");
+
+		String lastName = getUserInput(scanner, "Enter last name: ", s -> s.matches("[A-Za-z]+"), "Invalid last name!");
+
+		String email = getUserInput(scanner, "Enter email: ",
+				s -> s.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}"), "Invalid email!");
+
+		String mobile = getUserInput(scanner, "Enter mobile number: ", s -> s.matches("\\d{10}"),
+				"Invalid mobile number!");
+
+		String password = getUserInput(scanner, "Enter password: ",
+				s -> s.matches("^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).{8,}$"), "Invalid password!");
+
+		System.out.println("First name: " + firstName);
+		System.out.println("Last name: " + lastName);
+		System.out.println("Email: " + email);
+		System.out.println("Mobile number: " + mobile);
+		System.out.println("Password: " + password);
 	}
 
-	public void validateLastName(String lastName) throws InvalidLastNameException {
-		if (lastName.isEmpty()) {
-			throw new InvalidLastNameException("Invalid last name");
-		}
-	}
-
-	public void validateEmail(String email) throws InvalidEmailException {
-		Matcher matcher = pattern.matcher(email);
-		if (!matcher.matches()) {
-			throw new InvalidEmailException("Invalid email");
-		}
-	}
-
-	public void validateMobile(String mobile) throws InvalidMobileException {
-		if (mobile.length() != 10) {
-			throw new InvalidMobileException("Invalid mobile");
-		}
-	}
-
-	public void validatePassword(String password) throws InvalidPasswordException {
-		if (password.length() < 8) {
-			throw new InvalidPasswordException("Invalid password");
-		}
-	}
-}
-
-class InvalidFirstNameException extends Exception {
-	public InvalidFirstNameException(String message) {
-		super(message);
-	}
-}
-
-class InvalidLastNameException extends Exception {
-	public InvalidLastNameException(String message) {
-		super(message);
-	}
-}
-
-class InvalidEmailException extends Exception {
-	public InvalidEmailException(String message) {
-		super(message);
-	}
-}
-
-class InvalidMobileException extends Exception {
-	public InvalidMobileException(String message) {
-		super(message);
-	}
-}
-
-class InvalidPasswordException extends Exception {
-	public InvalidPasswordException(String message) {
-		super(message);
+	private static String getUserInput(Scanner scanner, String prompt, Predicate<String> validator, String errorMsg) {
+		String input;
+		boolean valid;
+		do {
+			System.out.print(prompt);
+			input = scanner.nextLine();
+			valid = validator.test(input);
+			if (!valid) {
+				System.out.println(errorMsg);
+			}
+		} while (!valid);
+		return input;
 	}
 }

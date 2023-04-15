18961e7c092f95e7a96e2f9221d9f8dde219fe06
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-04-15 11:43:28 +0530

    add: to check the valid mobile number

--- src/main/java/com/bridgelabz/userresitrationproblem/UserResistration.java
+++ src/main/java/com/bridgelabz/userresitrationproblem/UserResistration.java
@@ -6,22 +6,21 @@ import java.util.regex.Pattern;
 public class UserResistration {
 
 	public static void main(String[] args) {
-		String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+		String regex = "^[1-9][0-9]\s[1-9][0-9]{9}$";
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Email Address: ");
-		String data = sc.nextLine();
+		System.out.println("Enter Mobile Number: ");
+		String mobileNO = sc.nextLine();
 
 		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(data);
+		Matcher matcher = pattern.matcher(mobileNO);
 		boolean result = matcher.matches();
 
 		if (result) {
-			System.out.println("Valid Email");
+			System.out.println("Valid Mobile Number");
 		} else {
-			System.out.println("Invalid Email");
+			System.out.println("Invalid Mobile Number");
 		}
 
 	}
 }
-
